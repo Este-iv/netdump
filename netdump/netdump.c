7e7c332518774eef3f74596b7b0ad8d9a385298b
Este-iv <eserna@iastate.edu>
2018-07-19 02:42:35 -0500

    header = (struct... is having issues

--- netdump/netdump.c
+++ netdump/netdump.c
@@ -248,7 +248,7 @@ void raw_print(u_char *user, const struct pcap_pkthdr *h, const u_char *p)
 
 
 void
- ethernet_data(const u_char*p, u_int size){
+ ethernet_data(const u_char* p, u_int size){
    header = (struct APR_info*)(p + 14);
    APR_t* header = NULL;
 
